dc4c462773b834bb91df5e7a5cba9269c583c790
Nithya <nithyasetlur@gmail.com>
2024-10-29 17:18:11 +1100

    User Registration and Authentication

--- users/admin.py
+++ users/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-  list_display = ('user', 'first_name', 'surname', 'nickname')
-  search_fields = ('user_username', 'nickname')
-  list_filter = ('nickname')
-
+    list_display = ('user', 'first_name', 'surname', 'nickname')
+    search_fields = ('user__username', 'nickname')
+    list_filter = ('nickname',)
+    
 admin.site.register(Profile, ProfileAdmin)
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views #allows us to connect to views.py through path
 
 urlpatterns = [ #this block creates paths to the login and logout views
     path("", views.user, name="user"),
-    path("login", views.login_view, name="login"), #login view link
-    path("logout", views.logout_view, name="logout"), #logout view link
+    # path ('accounts/', include('allauth.urls')),
+    path("login/", views.login_view, name="login"), #login view link
+    path("logout/", views.logout_view, name="logout"), #logout view link
+    path('register/', views.register, name='register') #register view link
 ]
